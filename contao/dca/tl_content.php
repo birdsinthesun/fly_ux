2ae8face0ba249745048af4b281e8682c51c19be
birds in the sun <73081731+birdsinthesun@users.noreply.github.com>
2025-04-08 23:52:45 +0200

    Update tl_content.php

--- contao/dca/tl_content.php
+++ contao/dca/tl_content.php
@@ -14,6 +14,23 @@ $GLOBALS['TL_DCA']['tl_content']['config']['ptable'] = 'tl_page';
 $GLOBALS['TL_DCA']['tl_content']['config']['ctable'] = ['tl_content'];
 $GLOBALS['TL_DCA']['tl_content']['config']['dynamicPtable'] = true;
 $GLOBALS['TL_DCA']['tl_content']['config']['onload_callback'][]['tl_page'] = 'addBreadcrumb';
+$GLOBALS['TL_DCA']['tl_content']['config']['switchToEdit']                = true;
+$GLOBALS['TL_DCA']['tl_content']['config']['enableVersioning']            = true;
+$GLOBALS['TL_DCA']['tl_content']['config']['markAsCopy']                  = 'headline';
+
+'ptable'                      => 'tl_page',
+		'ctable'                      => array('tl_content'),
+		'dynamicPtable'               => true,
+        'switchToEdit'                => true,
+		'enableVersioning'            => true,
+		'markAsCopy'                  => 'title',
+		'onload_callback'             => array
+		(
+			array('tl_content', 'adjustDcaByType'),
+			array('tl_content', 'showJsLibraryHint'),
+            array('tl_page', 'addBreadcrumb')
+		),
+
 $GLOBALS['TL_DCA']['tl_content']['list']['sorting']['mode'] = DataContainer::MODE_TREE_EXTENDED;
 $GLOBALS['TL_DCA']['tl_content']['list']['label']['fields'] =  ['headline', 'type', 'inColumn'];
 $GLOBALS['TL_DCA']['tl_content']['list']['label']['format'] =   '%s <span class="label-info">[%s]</span><span class="label-column"> %s </span>';
