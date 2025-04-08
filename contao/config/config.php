c14cd05385be1e58091d8d3bd8125a04aacf9b2e
birds in the sun <73081731+birdsinthesun@users.noreply.github.com>
2025-04-08 23:43:12 +0200

    Update config.php

--- contao/config/config.php
+++ contao/config/config.php
@@ -2,6 +2,7 @@
 use Contao\CoreBundle\ContaoCoreBundle;
 
 unset($GLOBALS['BE_MOD']['content']['article']);
+unset($GLOBALS['FE_MOD']['navigationMenu']['articlenavigation']);
 $GLOBALS['BE_MOD']['content']['content'] = [
     'tables' => ['tl_page', 'tl_content'],
     'icon'   => 'vendor/birdsinthesun/fly_ux/assets/icon.svg', // optional
