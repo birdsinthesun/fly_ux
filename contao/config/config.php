bf4514ccc677e91a4e8fda0f37b149e53813eff6
birds in the sun <73081731+birdsinthesun@users.noreply.github.com>
2025-04-11 01:18:10 +0200

    dev

--- contao/config/config.php
+++ contao/config/config.php
@@ -12,7 +12,7 @@ unset($GLOBALS['FE_MOD']['navigationMenu']['articlenav']);
 unset($GLOBALS['FE_MOD']['miscellaneous']['articlelist']);
 unset($GLOBALS['TL_CTE']['includes']['article']);
 unset($GLOBALS['TL_CTE']['includes']['alias']);
-unset($GLOBALS['TL_MODELS']['tl_article']);
+//unset($GLOBALS['TL_MODELS']['tl_article']);
 
 $GLOBALS['BE_MOD']['content']['content'] = [
     'tables' => ['tl_content'],
--- src/EventListener/AddBackendAssetsListener.php
+++ src/EventListener/AddBackendAssetsListener.php
@@ -3,30 +3,32 @@
 namespace Bits\FlyUxBundle\EventListener;
 
 // src/EventListener/AddBackendAssetsListener.php
-namespace App\EventListener;
 
 use Contao\CoreBundle\Routing\ScopeMatcher;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
 
-#[AsEventListener]
+#[AsEventListener(event: KernelEvents::REQUEST, priority: 15)]
 class AddBackendAssetsListener
 {
-    public function __construct(private readonly ScopeMatcher $scopeMatcher)
+    private ScopeMatcher $scopeMatcher;
+
+    
+    public function __construct(ScopeMatcher $scopeMatcher)
     {
+        $this->scopeMatcher = $scopeMatcher;
     }
 
     public function __invoke(RequestEvent $event): void
     {
-        
-        var_dump('test',RequestEvent->get('do') );exit;
+        var_dump('test', $event->getRequest()->get('do')); exit;
+
         if (!$this->scopeMatcher->isBackendMainRequest($event)) {
             return;
         }
-        if (RequestEvent->get('do') === 'files' && RequestEvent->get('view') === 'media') {
-      
-            $GLOBALS['TL_CSS'][] = 'bundles/birdsinthesun/fly_ux/assets/dc_media.css'/* add your CSS asset here */;
-      //  $GLOBALS['TL_JAVASCRIPT'][] = /* add your JS asset here */;
+        if ($event->getRequest()->get('do') === 'files' && $event->getRequest()->get('view') === 'media') {
+            $GLOBALS['TL_CSS'][] = 'bundles/birdsinthesun/fly_ux/assets/dc_media.css';
         }
     }
 }
