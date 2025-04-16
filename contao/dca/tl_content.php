c7d86124063133ebf9f6ba928836d5813b66bc40
birds in the sun <73081731+birdsinthesun@users.noreply.github.com>
2025-04-16 13:21:24 +0200

    Update tl_content.php

--- contao/dca/tl_content.php
+++ contao/dca/tl_content.php
@@ -31,7 +31,10 @@ $GLOBALS['TL_DCA']['tl_content']['list']['label']['fields'] =  ['headline', 'typ
 $GLOBALS['TL_DCA']['tl_content']['list']['label']['format'] =   '%s <span class="label-info">[%s]</span><span class="label-column"> %s </span>';
 //$GLOBALS['TL_DCA']['tl_content']['list']['label']['label_callback'] =   array('tl_content', 'addIcon');
 
-
+unset($GLOBALS['TL_CTE']['includes']['article']);
+unset($GLOBALS['TL_CTE']['includes']['alias']);
+unset($GLOBALS['TL_CTE']['legacy']['accordionSingle']);
+unset($GLOBALS['TL_CTE']['miscellaneous']['swiper']);
 unset($GLOBALS['TL_DCA']['tl_content']['list']['sorting']['fields']);
 unset($GLOBALS['TL_DCA']['tl_content']['list']['sorting']['panelLayout']);
 unset($GLOBALS['TL_DCA']['tl_content']['list']['sorting']['defaultSearchField']);
