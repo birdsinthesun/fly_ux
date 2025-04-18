fe0084cd5b4dd43108edc304d1d7a055e05387cd
birds in the sun <73081731+birdsinthesun@users.noreply.github.com>
2025-04-18 20:24:42 +0200

    debug

--- config/bundles.php
+++ config/bundles.php
@@ -2,5 +2,6 @@
 
 return [
     // Andere Bundles
+     Contao\CoreBundle\ContaoCoreBundle::class => ['all' => true],
     Bits\FlyUxBundle\FlyUxBundle::class => ['all' => true],
 ];
--- src/FlyUxBundle.php
+++ src/FlyUxBundle.php
@@ -11,11 +11,18 @@ use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 use Bits\FlyUxBundle\DependencyInjection\Compiler\RemoveContaoCallbackPass;
 use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 
-class FlyUxBundle extends Bundle
+class FlyUxBundle extends AbstractBundle
 {
     
      public function build(ContainerBuilder $container): void
     {
+        
+        //  $projectDir = $container->getParameter('kernel.project_dir').'/vendor/birdsinthesun/fly_ux';
+        //$loader = new YamlFileLoader($container, new FileLocator($projectDir.'/config')); 
+        //$loader->load('services.yaml');
+        //$loader2 = new PhpFileLoader($container, new FileLocator($projectDir. '/config'));
+       
+       // $loader2->load('bundles.php');
         parent::build($container);
 
         $container->addCompilerPass(
@@ -25,12 +32,12 @@ class FlyUxBundle extends Bundle
         100
         );
         
-        $projectDir = $container->getParameter('kernel.project_dir');
-        $loader = new YamlFileLoader($container, new FileLocator($projectDir.'/config')); 
-        $loader->load('services.yaml');
-        $loader2 = new PhpFileLoader($container, new FileLocator($projectDir. '/config'));
-       
-        $loader2->load('bundles.php');
+      
+    }
+    protected function addCompilerPass($compilerPass, $type = PassConfig::TYPE_AFTER_REMOVING, $priority = 0)
+    {
+        // Dies stellt sicher, dass der CompilerPass korrekt registriert wird.
+        $this->container->addCompilerPass($compilerPass, $type, $priority);
     }
 
 }
