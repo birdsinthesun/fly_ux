54e69ac12e856b6261ee5ef03c1ab0b4fdebbf86
birds in the sun <73081731+birdsinthesun@users.noreply.github.com>
2025-08-27 11:01:11 +0200

    dev

---
+++ contao/languages/de/tl_settings.php
@@ -0,0 +1,5 @@
+<?php
+
+$GLOBALS['TL_LANG']['CHMOD']['editarticles'] = 'Inhalte bearbeiten';
+$GLOBALS['TL_LANG']['CHMOD']['movearticles'] = 'Hierarchie der Inhalte ändern.';
+$GLOBALS['TL_LANG']['CHMOD']['deletearticles'] = 'Inhalte löschen';
